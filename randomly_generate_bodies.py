68c2b83baa6ebb2f05f8419ff853c1cbf45d7ac7
Elli Beres <ellifteria@gmail.com>
2023-02-13 11:37:49 -0600

    generating semi-random body plans

---
+++ randomly_generate_bodies.py
@@ -0,0 +1,36 @@
+import os
+import random
+from build_body import *
+import simulate_recursive_body
+import pybullet as pblt
+
+for i in range(10):
+    solution_id = 0
+
+    body_plan = BodyCons(RandomSizedBodyPiece(),
+                         random.randint(1, 3),
+                         BodyCons(RandomSizedSensorPiece(),
+                                  random.randint(1, 3),
+                                  BodyCons(RandomSizedBodyPiece(),
+                                           random.randint(1, 3),
+                                           BodyCons(RandomSizedSensorPiece(),
+                                                    2,
+                                                    BodyCons(RandomSizedBodyPiece(),
+                                                             random.randint(1, 3),
+                                                             BodyCons(RandomSizedSensorPiece(),
+                                                                      random.randint(1, 3),
+                                                                      None))))))
+
+    pyrosim.Start_URDF("./data/robot/body{}.urdf".format(solution_id))
+
+    joint_names = build_body(body_plan)
+
+    pyrosim.End()
+
+    pyrosim.Start_NeuralNetwork("./data/robot/brain{}.nndf".format(solution_id))
+
+    build_brain(body_plan, joint_names)
+
+    pyrosim.End()
+
+    os.system('python simulate_recursive_body.py')
