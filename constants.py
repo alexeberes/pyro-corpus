4b53e4458fe0e63705da7a68c7f3b8782e69f7e0
Alex Beres <75806636+alexeberes@users.noreply.github.com>
2023-02-26 01:35:14 -0600

    Slices solutions into parts to simulate

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-num_iterations = 10000
+num_iterations = 150000
 
-sleep_time = 0.5/60
+sleep_time = 0.001/60
 
-num_generations = 10
+num_generations = 50
 
 motor_joint_range = .2
 
@@ -15,10 +15,12 @@ default_fitness = 100
 mutation_rate = 0.75
 mutation_magnitude = 2
 
-generation_size = 5
+generation_size = 4
 number_of_children = 5
 family_filter_size = 3
 random_members = 3
 
 CPG_magnitude = 2
 CPG_period_modifier = 2*np.pi
+
+num_simulations_at_once = 5
