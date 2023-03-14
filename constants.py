b8b2ffbdf09af253674c92fd6ce957b8e91e77b3
Elli Beres <ellifteria@gmail.com>
2023-03-14 12:33:04 -0500

    begining comparisons

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-num_iterations = 1000
+num_iterations = 10000
 
 sleep_time = 1/60
 
-num_generations = 1
+num_generations = 4
 
 motor_joint_range = .2
 
@@ -16,7 +16,7 @@ mutation_rate = 0.75
 mutation_magnitude = 2
 
 generation_size = 20
-number_of_children = 1
+number_of_children = 5
 family_filter_size = 3
 random_members = 3
 
--- search.py
+++ search.py
@@ -1,24 +1,35 @@
 import argparse
 from faery_pc1mp import FAERYvPyrCor1MP
+from faery_pc1nop import FAERYvPyrCor1NoP
 import os
 import matplotlib.pyplot as plt
 import pickle
+
+from timeit import default_timer as timer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog = 'search.py',
         description = 'Performs a search for an optimizied neural network weight set for a simulated robot using FAEry Algorithms')
-    parser.add_argument('-m', '--method', choices=['1'], default='1')
+    parser.add_argument('-m', '--method', choices=['1', '2'], default='1')
 
     args = parser.parse_args()
 
     if args.method == '1':
         evolutionary_algorithm = FAERYvPyrCor1MP()
+    elif args.method == '2':
+        evolutionary_algorithm = FAERYvPyrCor1NoP()
     else:
         exit()
 
+    start_time = timer()
 
     best_genome_file_name, fitnesses_file_name = evolutionary_algorithm.evolve()
+
+    end_time = timer()
+    
+    elapsed_time = end_time - start_time
+    print('\n\nexecution time: ', elapsed_time, 'seconds')
 
     cmd_to_send = 'python simulate_genome.py -f "{}"'.format(best_genome_file_name)
 
